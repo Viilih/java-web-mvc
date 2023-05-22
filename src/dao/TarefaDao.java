9d0a15fc4b389d3da4195f51c8214cc9a0493563
Viilih <gnlnascimento@gmail.com>
2023-05-22 13:39:39 -0300

    dao task

---
+++ src/dao/TarefaDao.java
@@ -0,0 +1,49 @@
+package dao;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Tarefa;
+
+public class TarefaDao {
+
+    // Fazer a conexão com o banco de dados
+    private Connection connection;
+    
+
+    public void AdicionarTarefa(Tarefa tarefa){
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    public List<Tarefa> ListarTarefas(){
+        List<Tarefa> tarefas = new ArrayList<>();
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        return tarefas;
+    }
+
+    public void AtualizarTarefa(){
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    public void ExcluirTarefa (int id){
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+    
+}
